ffb6432ffc68977e090b55f860dd35d66fab5dae
Elena Williams <elena.sivanjali.williams@gmail.com>
2025-01-24 05:30:19 -0800

    removed unnecessary API call

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ import dash_bootstrap_components as dbc
 
 
 # GLOBAL VARIABLES ----
-API_KEY = "yLKJat5uDtkVnA5UkLNwGQVWUjg99Prg"
 ELEMENTS = ['H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og']
 CRYSTAL_SYSTEMS = ["Cubic", "Orthorhombic", "Monoclinic", "Tetragonal", "Hexagonal"]
 POINT_GROUPS = ['6/mmm', 'm-3m', '-42m', '6/m', '4/mmm', '6mm', '-43m', '2/m',
