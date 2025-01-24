9f368ee895f0726f7335a95cdcf822b570a0a360
Elena Williams <elena.sivanjali.williams@gmail.com>
2025-01-24 05:50:02 -0800

    reorganized for dashtools

--- src/app.py
+++ src/app.py
@@ -34,13 +34,13 @@ def costFunction1d(a_mismatch, mcia, large_superlattice=0.5):
     return np.abs(a_mismatch) * (mcia**(1-large_superlattice))
 
 # SUBSTRATE / FILM DATA ----
-sublattices_2d = pd.read_csv("data/sublattices_2d.csv")
-sublattices_1d = pd.read_csv("data/sublattices_1d.csv")
+sublattices_2d = pd.read_csv("./data/sublattices_2d.csv")
+sublattices_1d = pd.read_csv("./data/sublattices_1d.csv")
 
 substrates = pd.concat([sublattices_2d["substrate"], sublattices_1d["substrate"]]).unique()
 
-films_2d = pd.read_csv("data/stable_films_2d.csv")
-films_1d = pd.read_csv("data/stable_films_1d.csv")
+films_2d = pd.read_csv("./data/stable_films_2d.csv")
+films_1d = pd.read_csv("./data/stable_films_1d.csv")
 films = pd.concat([films_2d["name"] + " " +  films_2d["crystal_system"], films_1d["name"] + " " +  films_1d["crystal_system"]]).unique()
 
     
@@ -399,6 +399,7 @@ def getLayout():
 
 # SERVE APP ----
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
 app.title = "Lattice Matcher | Falson Lab"
 app.layout = getLayout()
 
